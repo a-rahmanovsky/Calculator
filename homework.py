f8ec50e6d10919ef1a7ab18fc8139b1846a41cfb
Андрей Рахмановский <a.rahmanovsky@yandex.ru>
2020-04-06 13:25:49 +0300

    afther second review

--- homework.py
+++ homework.py
@@ -24,15 +24,15 @@ class CashCalculator(Calculator):
 
     def __init__(self, limit):
         super().__init__(limit)
-        self.code2data = {
+        self.data_by_currency_code = {
             'rub': (1, "руб"),
             'usd': (self.USD_RATE, "USD"),
             'eur': (self.EURO_RATE, "Euro")
         }
 
     def __convert_cash(self, cash, currency):
-        actual_cash = round(cash / self.code2data[currency][0], 2)
-        name = self.code2data[currency][1]
+        rate, name = self.data_by_currency_code[currency]
+        actual_cash = round(cash / rate, 2)
         return f"{actual_cash} {name}"
 
     def get_today_cash_remained(self, currency):
@@ -54,18 +54,10 @@ class CaloriesCalculator(Calculator):
         balance = self.limit - self.get_today_stats()
         if balance > 0:
             return f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {balance} кКал"
-        else:
-            return "Хватит есть!"
+        return "Хватит есть!"
 
 class Record:
     def __init__(self, amount, comment='', date=dt.datetime.now().date()):
         self.amount = amount
         self.comment = comment
         self.date = date if date == dt.datetime.now().date() else dt.datetime.strptime(date, '%d.%m.%Y').date()
-
-
-calc = CashCalculator(1000)
-calc.add_record(Record(amount=145, comment="кофе"))
-# и к этой записи тоже дата должна добавиться автоматически
-calc.add_record(Record(amount=300, comment="Серёге за обед", date="03.04.2020"))
-print(calc.get_week_stats())
